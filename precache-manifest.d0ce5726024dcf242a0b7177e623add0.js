00ddb4ccbd369351f0c3952e9a0fb1b25ba826ed
Divyanshi Singh <singhdivyanshi797@gmail.com>
2020-06-02 23:33:01 +0530

    Updates

---
+++ precache-manifest.d0ce5726024dcf242a0b7177e623add0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6f64872cf4f60ac1c8ba8416a31c35b",
+    "url": "/reactP/index.html"
+  },
+  {
+    "revision": "b3d907353ed7d20bdf4b",
+    "url": "/reactP/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "1eccaeb928502e194574",
+    "url": "/reactP/static/css/main.35641b4a.chunk.css"
+  },
+  {
+    "revision": "b3d907353ed7d20bdf4b",
+    "url": "/reactP/static/js/2.cb8a07e9.chunk.js"
+  },
+  {
+    "revision": "618d9e067b5f7c0ee048a43ae1109b7d",
+    "url": "/reactP/static/js/2.cb8a07e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1eccaeb928502e194574",
+    "url": "/reactP/static/js/main.a02339b9.chunk.js"
+  },
+  {
+    "revision": "4cec13bf7f73fcee6b14",
+    "url": "/reactP/static/js/runtime-main.8d50abda.js"
+  },
+  {
+    "revision": "5187a7a6996cba4d18f13c08e833be01",
+    "url": "/reactP/static/media/box5img2.5187a7a6.jfif"
+  },
+  {
+    "revision": "2f8776a94befff77d4c692266fb2be79",
+    "url": "/reactP/static/media/box8img.2f8776a9.jfif"
+  },
+  {
+    "revision": "0cbe1b4d4c1ad4560c8c9a75942a70d4",
+    "url": "/reactP/static/media/box9img.0cbe1b4d.jfif"
+  },
+  {
+    "revision": "82ddea0affc73f27592eb484ed36b2ec",
+    "url": "/reactP/static/media/clock2.82ddea0a.jpg"
+  },
+  {
+    "revision": "d19c226ca61021accc769b37b7d4c5bd",
+    "url": "/reactP/static/media/pimg1.d19c226c.jfif"
+  },
+  {
+    "revision": "a9a5228cbd55c711c551279d10d4e12c",
+    "url": "/reactP/static/media/twitterimg.a9a5228c.jfif"
+  }
+]);
